da861767ecfaa551d37a62cc8689df7c8b9a4626
RadKirill <radkirill00@mail.ru>
2024-02-27 21:34:04 +0300

    qwe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,14 +12,11 @@ const Home = () => {
             <li className="content-list__item">
               <h2 className="title-2">Frontend</h2>
               <p>
-                JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM,
-                BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+                JavaScript, TypeScript, ReactJS, HTML, CSS, NPM,
+                BootStrap, StyledComponents, GIT,  React Hooks, React Router.
               </p>
             </li>
-            <li className="content-list__item">
-              <h2 className="title-2">Backend</h2>
-              <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-            </li>
+
           </ul>
         </div>
       </main>
