e96f4034b82391eafac41e2f6c47bb547aa0578d
RadKirill <radkirill00@mail.ru>
2024-03-03 17:35:08 +0300

    qwe

--- qwe.js
+++ qwe.js
@@ -1,1 +1,10 @@
+let elem = document.getElementById("elem");
 
+function func(name, surname) {
+  console.log(this.value + ", " + name + " " + surname);
+}
+
+let newfunc = func.bind(elem);
+
+newfunc("John", "Smit"); 
+newfunc("Eric", "Luis"); 
